1f5e3b03d0da8e1ad319c4bbaf54218f00697b90
AngeTia <tiaange03@gmail.com>
2023-02-10 19:00:58 +0000

    Update Content

--- Exercise2/storeLoginDetail.php
+++ Exercise2/storeLoginDetail.php
@@ -1,43 +1,55 @@
-<?php
-    // Stocker les détails de connexion en utilisant un cookie
-    setcookie("username", "AngeTia", time() + (86400 * 30), "/"); // 86400 = 1 jour
 
-    // Déclarer les variables $submit et $logIn et les initialiser à false
+
+<?php
+    // my user data
+    $data = [
+        "username" => "michael",
+        "cookiesTime" => time() + (86400 * 30),
+    ];
+
+    // encode data in json and base64
+    $dataEncoding = base64_encode(json_encode($data));
+
+    // Store Login Detail
+    setcookie("userInfos", $dataEncoding, time() + (86400 * 30), "/"); // 86400 = 1 day
+
+    // form submit
     $submit = false;
+    // login state
     $logIn = false;
 
-    // Soumettre le formulaire
+    // submit form
     if ($_SERVER["REQUEST_METHOD"] == "POST") {
-        // Définir la variable $submit sur true si le formulaire est soumis
         $submit = true;
 
-        // Vérifier si le nom d'utilisateur stocké dans le cookie correspond à celui soumis dans le formulaire
-        if (($_COOKIE["username"] == $_POST["username"])) {
-            // Si les noms d'utilisateur correspondent, définir $logIn sur true
+        // decode data base64 and json
+        $dataDecrypt = json_decode(base64_decode($_COOKIE["userInfos"]), true);
+
+        if (($dataDecrypt["username"] == $_POST["username"])) {
             $logIn = true;
         }
     }
 ?>
+
 <!DOCTYPE html>
 <html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Exercise 1</title>
-    </head>
-    <body>
-        <!-- Formulaire de connexion -->
-        <form action="<?= htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
-            <label for="name">Username</label>
-            <input type="text" name="username" id="username">
-            <input type="submit" value="Valider">
-        </form>
-    <!-- Message de connexion -->
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Exercise 1</title>
+</head>
+<body>
+    <form action="<?= htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
+        <label for="name">Username</label>
+        <input type="text" name="username" id="username">
+        <input type="submit" value="Valider">
+    </form>
+
+    <!-- msg connect -->
     <?php if($submit): ?>
         <h3>Login <?= $logIn ? "success" : "error" ?> </h3>
     <?php endif; ?>
-    </body>
+
+</body>
 </html>
-
-
